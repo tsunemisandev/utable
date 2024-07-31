b152566d6d1ce2007e55dfdd558b7727b7c16830
tsunemisan <tsunemi.dev@gmail.com>
2024-08-01 00:11:45 +0900

    By DOM

--- vue2-umy-table/src/App.vue
+++ vue2-umy-table/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { ref, nextTick } from 'vue';
+import { ref, nextTick, watch, onMounted } from 'vue';
 const multipleTable = ref(null)
+const multipleTable2 = ref(null)
 const multipleSelection = ref([])
 const allSelected = ref(false)
 const tableData = ref([])
@@ -9,42 +10,94 @@ tableData.value = [
   { name: 'Douglas', address: 'Sao Paulo' }
 ]
 
+const selectSpecific = () => {
+  multipleTable.value.clearSelection()
+  tableData.value.forEach(row => {
+    if (row.name === 'Thiago') {
+      multipleTable.value.toggleRowSelection([{ row: row, selected: true }])
+    }
+  })
+}
+
 const selectAll = (rows) => {
-  console.log(multipleTable.value)
-  if (!allSelected.value) {
+  // console.log(multipleTable.value)
+  // if (!allSelected.value) {
+  //   multipleTable.value.clearSelection()
+  //   rows.forEach((row, index) => {
+  //     if (row.name === 'Thiago') {
+  //       multipleTable.value.toggleRowSelection([{ row: row, selected: true }])
+  //     }
+  //   })
+  // } else {
+
+  //   multipleTable.value.clearSelection()
+
+  // }
+  // allSelected.value = !allSelected.value
+
+  const dom = document.getElementById('multipleTable').querySelector('.el-table-column--selection .el-checkbox__input')
+  
+  if(dom.classList[1] === 'is-focus'){
     multipleTable.value.clearSelection()
     rows.forEach((row, index) => {
       if (row.name === 'Thiago') {
         multipleTable.value.toggleRowSelection([{ row: row, selected: true }])
       }
     })
-  } else {
-
-    multipleTable.value.clearSelection()
-
   }
-  allSelected.value = !allSelected.value
 }
 
 function handleSelectionChange(val) {
   multipleSelection.value = val;
   // console.log(multipleSelection.value.length)
+  // console.log(multipleTable)
+}
+
+onMounted(() => {
+  // document.getElementById('multipleTable2').querySelector('.el-checkbox__input')
+  // nextTick(() => {
+  //   document.getElementById('multipleTable2').querySelectorAll('.el-table-column--selection input[type="checkbox"]').forEach(item => item.addEventListener('click', () => {
+  //     const dom = document.getElementById('multipleTable2').querySelector('.el-checkbox__input')
+  //     console.log(dom.classList)
+  //     if (dom.classList[1] == 'is-focus') {
+  //       selectAll2()
+  //     }
+  //   }))
+  // })
+
+})
+
+const selectAll2 = async (rows) => {
+
+  console.log('Entered')
+  multipleTable2.value.clearSelection()
+  tableData.value.forEach((row, index) => {
+    if (row.name === 'Thiago') {
+      multipleTable2.value.toggleRowSelection([{ row: row, selected: false }])
+    }
+  })
+
 }
 
 </script>
 
 <template>
-  <div id="app">
-    <u-table ref="multipleTable" :data="tableData" style="width: 100%" @select-all="selectAll"
-      @selection-change="handleSelectionChange" key="index" empty-text="No data">
-      <u-table-column type="selection" width="55">
-      </u-table-column>
+  <div id="app" style="display: block;">
+    <!-- ボタンで選択 -->
+    <span> ボタンで選択</span>
+    <div><button @click="selectSpecific">特定のみ選択</button></div>
+    <div>
+      <u-table ref="multipleTable" id="multipleTable" :data="tableData" style="width: 100%" @select-all="selectAll"
+        @selection-change="handleSelectionChange" key="index" empty-text="No data">
+        <u-table-column type="selection" width="55">
+        </u-table-column>
 
-      <u-table-column property="name" label="Name" width="120">
-      </u-table-column>
-      <u-table-column property="address" label="Address" show-overflow-tooltip>
-      </u-table-column>
-    </u-table>
+        <u-table-column property="name" label="Name" width="120">
+        </u-table-column>
+        <u-table-column property="address" label="Address" show-overflow-tooltip>
+        </u-table-column>
+      </u-table>
+    </div>
   </div>
 </template>
 
